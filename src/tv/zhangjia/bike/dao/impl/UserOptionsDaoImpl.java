5a6710cbd94034aadf41c91b5ebfc85120a88561
zhangjia <zhangjia@188.com>
2019-04-25 22:24:37 +0800

    JDBC：UserOptionsDaoImpl 替换完成v1.0

--- src/tv/zhangjia/bike/dao/impl/UserOptionsDaoImpl.java
+++ src/tv/zhangjia/bike/dao/impl/UserOptionsDaoImpl.java
@@ -1,82 +1,42 @@
 package tv.zhangjia.bike.dao.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import tv.zhangjia.bike.dao.UserOptionsDao;
 import tv.zhangjia.bike.entity.UserOptions;
 import tv.zhangjia.bike.util.CommonDao;
 
 public class UserOptionsDaoImpl extends CommonDao implements UserOptionsDao {
-//	List<UserSettings> userSettings = Database.USERSETTINGS;
-//	List<UserOptions> userOptions = Database.USEROPTIONS;
-
-//	@Override
-//	public int doInsert(UserOptions us) {
-//		us.setId(Database.nextUserOptionsId());
-//		userOptions.add(us);
-//		return us.getId();
-//	}
+	/**
+	 * 添加用户设置
+	 * @param us 用户设置对象
+	 * @return 添加成功返回1，添加不成功返回0
+	 */
 	@Override
 	public int doInsert(UserOptions us) {
 		String sql = "INSERT INTO user_options VALUES(seq_ups.nextval,?,?,?)";
-		return executeUpdate(sql, us.getUserId(),us.getName(),us.getValue());
+		return executeUpdate(sql, us.getUserId(), us.getName(), us.getValue());
 	}
 
-
-//
-//	@Override
-//	public String queryUserSetting(int id,String name) {
-//		for (UserOptions userOptions2 : userOptions) {
-//			if(userOptions2.getUserId() == id && userOptions2.getName().equals(name)) {
-//				return userOptions2.getValue();
-//			}
-//		}
-//		return null;
-//	}
-
+	/**
+	 * 查询用户设置
+	 * @param id 要查询的用户id
+	 * @param name 要查询的设置名称
+	 * @return 要查询的设置所对应的值
+	 */
 	@Override
-	public String queryUserSetting(int id,String name) {
+	public String queryUserSetting(int id, String name) {
 		String sql = "SELECT * FROM user_options WHERE user_id = ? AND name=?";
-		return query4Bean(sql, id,name).getValue();
+		return query4Bean(sql, UserOptions.class, id, name).getValue();
 	}
-
-//	@Override
-//	public boolean sestValues(int userId,String name, String values) {
-//		for (UserOptions userOptions2 : userOptions) {
-//			if(userOptions2.getName().equals(name) && userOptions2.getUserId() == userId) {
-//				userOptions2.setValue(values);
-//			}
-//		}
-//		return false;
-//	}
-//
-//	@Override
-//	public boolean setOptions(int userId, String name, String values) {
-//		for (UserOptions u2 : userOptions) {
-//			if(u2.getUserId()==userId && u2.getName().equals(name)) {
-//				u2.setValue(values);
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-
 	
-
-	@Override
-	public UserOptions getBeanFromResultSet(ResultSet rs) throws SQLException {
-		UserOptions us  = new UserOptions(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4));
-		return us;
-	}
-
-
+	/**
+	 * 更新用户设置
+	 * @param us 用户设置对象
+	 * @return 更新成功返回1，更新失败返回0
+	 */
 	@Override
 	public int doUpdate(UserOptions us) {
 		String sql = "UPDATE user_options SET value=? WHERE user_id=? AND name=?";
-		
-		return executeUpdate(sql, us.getValue(),us.getUserId(),us.getName());
+		return executeUpdate(sql, us.getValue(), us.getUserId(), us.getName());
 	}
-		
 
 }
